ca9c0f5ab6749349eb39451a45379d75ca0a5aea
Chad Rosser <rosserchad@gmail.com>
2024-01-16 07:49:03 -0500

    testing github actions

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,7 +13,11 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Hey check it out mom and dad, I did it!</h1>
+        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
         <Counter count={count} />
       </div>
     </div>
