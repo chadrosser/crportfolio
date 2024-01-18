ba443070fbccbd0a53bbbbe29888bbf0b99c57b2
Chad Rosser <rosserchad@gmail.com>
2024-01-18 16:12:06 -0500

    added nav bar and cleared home page

---
+++ components/SideBar.tsx
@@ -0,0 +1,37 @@
+import { FaGithub, FaLinkedin, FaHome } from "react-icons/fa";
+import { GiGuitarBassHead, GiSmart } from "react-icons/gi";
+import { GoRocket, GoVersions, GoFile } from "react-icons/go";
+import { IoMdContact } from "react-icons/io";
+
+
+
+
+export default function SideBar() {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+            <a href = "/"><SideBarIcon icon={<FaHome size="28" />} text={"Home"} /></a>
+            <Divider />
+            <a href = "/about"><SideBarIcon icon={<GiGuitarBassHead size="28" />} text={"About Me"} /></a>
+            <a href = "/career"><SideBarIcon icon={<GoRocket size="28" />} text={"Career"} /></a>
+            <a href = "/education"><SideBarIcon icon={<GiSmart size="32" />} text={"Education"} /></a>
+            <a href = "/resume"><SideBarIcon icon={<GoFile size="28" />} text={"Resume"} /></a>
+            <a href = "/contact"><SideBarIcon icon={<IoMdContact size="32" />} text={"Contact"} /></a>
+            <a href = "/how"><SideBarIcon icon={<GoVersions size="32" />} text={"How I made this site"} /></a>
+            <a href="https://www.github.com/chadrosser"><SideBarIcon icon={<FaGithub size="28" />} text={"GitHub"} /></a>
+            <a href="https://www.linkedin.com/in/chadrosser/"><SideBarIcon icon={<FaLinkedin size="28" />} text={"LinkedIn"} /></a>
+        </div>
+    )
+}
+
+// deno-lint-ignore no-explicit-any
+const SideBarIcon = ({ icon, text }: any) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+);
+
+const Divider = () => <hr className="sidebar-hr" />;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,29 +1,25 @@
+import SideBar from '../components/SideBar.tsx';
 
 
 export default function Home() {
   return (
-    <div class="flex min-h-screen flex-col p-12 bg-neutral-900">
-      <nav className="grid grid-cols-3 items-center">
-          <h1 className="text-xl font-semibold text-white">
-            Chad Rosser
-          </h1>
-          <span className="justify-self-center text-blue-600 font-mono">
-            Github | LinkedIn
-          </span>
-      </nav>
+    <div class="flex min-h-screen flex-col p-12 bg-gray-900">
+      <div className="flex">
+        <SideBar />
+      </div>
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/beli.PNG"
+          src="/construction.jpg"
           width="500"
           height="500"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="what"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-        </p>
       </div>
-    </div>
+    </div>      
   );
 }
+
+
+// navbar -  career, education, resume?, contact, making of this site, 
+
